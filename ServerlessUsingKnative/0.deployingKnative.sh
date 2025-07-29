0bde95c0945af5d4299d479ebe9f2ba2934e7ff1
naman.goel <naman.goel@groundzero.cloud>
2025-07-29 11:18:45 +0530

    getting started with knative

---
+++ ServerlessUsingKnative/0.deployingKnative.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#deploying knative serving CRDs
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.18.1/serving-crds.yaml
+
+#deploying knative serving core
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.18.1/serving-core.yaml
+
+#installing the network layer (kourier) and configuring knative serving to use it
+    #installing kourier
+    kubectl apply -f https://github.com/knative/net-kourier/releases/download/knative-v1.18.0/kourier.yaml
+
+    #configuring knative serving to use kourier
+    kubectl patch configmap/config-network \
+    --namespace knative-serving \
+    --type merge \
+    --patch '{"data":{"ingress-class":"kourier.ingress.networking.knative.dev"}}'
+
+    #Fetch the External IP address or CNAME
+    kubectl --namespace kourier-system get service kourier
+
+#Verify the installation
+kubectl get pods -n knative-serving
+
+#Configuring DNS (Magic DNC (sslip.io))
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.18.1/serving-default-domain.yaml
